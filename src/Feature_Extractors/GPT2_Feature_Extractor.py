1e8cf254c6bd9ef0cf5509ff1b90a812f676f1dc
kunaldudhavat <kunal.dudhavat@gmail.com>
2024-04-28 10:20:58 -0400

    Changes for features extraction and device selection

--- src/Feature_Extractors/GPT2_Feature_Extractor.py
+++ src/Feature_Extractors/GPT2_Feature_Extractor.py
@@ -10,7 +10,7 @@ class GPT2_Feature_Extractor(Base_Feature_Extractor):
         super().__init__()
         self.tokenizer = transformers.AutoTokenizer.from_pretrained('gpt2-xl')
         self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
-        self.model = transformers.AutoModelForCausalLM.from_pretrained('gpt2-xl')
+        self.model = transformers.AutoModelForCausalLM.from_pretrained('gpt2-xl').to(self.device)
         self.byte_encoder = bytes_to_unicode()
         self.byte_decoder = {unicode_val: byte_key for byte_key, unicode_val in self.byte_encoder.items()}
 
@@ -19,7 +19,7 @@ class GPT2_Feature_Extractor(Base_Feature_Extractor):
         Extracts the features for the given text sequence based on the perplexities of the model GPT2 for the given sequence
         """
         tokens = self.tokenizer(txt, return_tensors='pt').to(self.device)
-        input_token_ids = labels = tokens.input_ids[:, :1024, ]
+        input_token_ids = labels = tokens.input_ids[:, :1024, ].to(self.device)
         outputs = self.model(input_token_ids)
         mean_loss, token_wise_loss_list = tokenwise_loss(outputs, labels)
         bytewise_loss_list = get_bytewise_loss(input_token_ids, token_wise_loss_list, self.tokenizer,
